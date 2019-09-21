0a0bbc49579dc999f423e932df3702a50cfb51fb
wqdchn <wqdong.chn@gmail.com>
2019-09-21 13:50:41 +0800

    算法40讲+LRU Cache+实现一个LRU Cache

---
+++ algorithm-40case/lru_cache.py
@@ -0,0 +1,56 @@
+# @program: PyDemo
+# @description: https://leetcode.com/problems/lru-cache/
+# Example:
+#
+# LRUCache cache = new LRUCache( 2 /* capacity */ );
+#
+# cache.put(1, 1);
+# cache.put(2, 2);
+# cache.get(1);       // returns 1
+# cache.put(3, 3);    // evicts key 2
+# cache.get(2);       // returns -1 (not found)
+# cache.put(4, 4);    // evicts key 1
+# cache.get(1);       // returns -1 (not found)
+# cache.get(3);       // returns 3
+# cache.get(4);       // returns 4
+# @author: wqdong
+# @create: 2019-09-21 13:30
+import collections
+
+# https://docs.python.org/zh-cn/3.7/library/collections.html?highlight=ordereddict#collections.OrderedDict
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.dic = collections.OrderedDict()
+        self.remain = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.dic:
+            return -1
+        v = self.dic.pop(key)
+        self.dic[key] = v
+        return v
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic:
+            self.dic.pop(key)
+        else:
+            if self.remain > 0:
+                self.remain -= 1
+            else:
+                self.dic.popitem(last=False)
+        self.dic[key] = value
+
+
+cache = LRUCache(2)
+
+cache.put(1, 1)
+cache.put(2, 2)
+print(cache.get(1))
+cache.put(3, 3)
+print(cache.get(2))
+cache.put(4, 4)
+print(cache.get(1))
+print(cache.get(3))
+print(cache.get(4))
